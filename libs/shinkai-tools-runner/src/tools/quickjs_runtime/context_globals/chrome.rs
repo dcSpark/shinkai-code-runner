09071357ef186ebcb37fb612af2bcb671dabc618
Nico Arqueros <1622112+nicarq@users.noreply.github.com>
2024-08-10 12:50:15 -0500

    add more methods

--- libs/shinkai-tools-runner/src/tools/quickjs_runtime/context_globals/chrome.rs
+++ libs/shinkai-tools-runner/src/tools/quickjs_runtime/context_globals/chrome.rs
@@ -1,7 +1,10 @@
+use base64::engine::general_purpose;
+use base64::Engine;
+use headless_chrome::protocol::cdp::Page::{self};
 use headless_chrome::{Browser, LaunchOptionsBuilder, Tab};
 use rquickjs::IntoJs;
-use rquickjs::{class::Trace, Class, Ctx, Object, Promise, Result, String as RQString, Value};
-use std::{borrow::Borrow, io, sync::Arc, time::Duration};
+use rquickjs::{class::Trace, Class, Ctx, Object, Promise, Result};
+use std::{io, sync::Arc};
 use tokio::sync::Mutex;
 
 #[derive(Trace)]
@@ -113,6 +116,74 @@ impl TabWrapper {
         });
         Ok(promise)
     }
+
+    pub fn wait_until_navigated<'js>(&self, ctx: Ctx<'js>) -> Result<Promise<'js>> {
+        let (promise, resolve, reject) = ctx.promise()?;
+        let ctx_clone = ctx.clone(); // Clone ctx here
+        let tab = Arc::clone(&self.tab); // Clone Arc to share ownership
+        ctx.spawn(async move {
+            let tab = tab.lock().await; // Await the lock
+            match tab.wait_until_navigated() {
+                Ok(_) => {
+                    let tab_wrapper = TabWrapper {
+                        tab: Arc::new(Mutex::new(Arc::clone(&tab))),
+                    };
+                    let tab_wrapper_value = tab_wrapper.into_js(&ctx_clone).unwrap();
+                    let tab_wrapper_object: Object = tab_wrapper_value.into_object().unwrap();
+                    resolve
+                        .call::<(Object,), ()>((tab_wrapper_object,))
+                        .unwrap();
+                }
+                Err(e) => {
+                    reject.call::<(String,), ()>((e.to_string(),)).unwrap();
+                }
+            }
+        });
+        Ok(promise)
+    }
+
+    pub fn capture_screenshot<'js>(
+        &self,
+        ctx: Ctx<'js>,
+        format: String,
+        quality: Option<u32>,
+        clip: Option<Object<'js>>,
+        from_surface: bool,
+    ) -> Result<Promise<'js>> {
+        let (promise, resolve, reject) = ctx.promise()?;
+        let tab = Arc::clone(&self.tab); // Clone Arc to share ownership
+        ctx.spawn(async move {
+            let tab = tab.lock().await; // Await the lock
+            let format = match format.as_str() {
+                "jpeg" => headless_chrome::protocol::cdp::Page::CaptureScreenshotFormatOption::Jpeg,
+                "png" => headless_chrome::protocol::cdp::Page::CaptureScreenshotFormatOption::Png,
+                "webp" => headless_chrome::protocol::cdp::Page::CaptureScreenshotFormatOption::Webp,
+                _ => {
+                    reject
+                        .call::<(String,), ()>(("Invalid format".to_string(),))
+                        .unwrap();
+                    return;
+                }
+            };
+            let clip = clip.map(|obj| Page::Viewport {
+                x: obj.get("x").unwrap_or(0.0),
+                y: obj.get("y").unwrap_or(0.0),
+                width: obj.get("width").unwrap_or(0.0),
+                height: obj.get("height").unwrap_or(0.0),
+                scale: obj.get("scale").unwrap_or(1.0),
+            });
+            match tab.capture_screenshot(format, quality, clip, from_surface) {
+                Ok(data) => {
+                    let base64_data = general_purpose::STANDARD.encode(data);
+                    resolve.call::<(String,), ()>((base64_data,)).unwrap();
+                }
+                Err(e) => {
+                    reject.call::<(String,), ()>((e.to_string(),)).unwrap();
+                }
+            }
+        });
+        Ok(promise)
+    }
 }
 
 pub fn init(ctx: &Ctx<'_>) -> Result<()> {
@@ -150,10 +221,16 @@ mod tests {
                 const browser = new HeadlessChrome('/Applications/Google Chrome.app/Contents/MacOS/Google Chrome');
                 console.log("Browser created", browser);
                 // const browser = new HeadlessChrome(null);
-                 const tab = await browser.create_new_tab();
-                 console.log("tab:", tab);
+                const tab = await browser.create_new_tab();
+                console.log("tab:", tab);
                 const tabWrapper = await tab.navigate_to('https://example.com');
+                await tabWrapper.wait_until_navigated();
                 const content = await tabWrapper.get_content();
+
+                   // Capture screenshot
+                const screenshot = await tabWrapper.capture_screenshot('png', null, null, true);
+                // console.log("Screenshot captured:", screenshot);
+
                 return { data: `Hello, ${params.name}! Content: ${content}` };
               } catch (error) {
                 return { error: error.message };
