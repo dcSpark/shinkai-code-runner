497828fd730a27e310fbaaf6d269f45c0ea5c4e8
Alfredo Gallardo <agallardol@outlook.com>
2024-08-18 01:18:36 -0400

    - fix: permissions for shinkai-tools-backend binary

--- libs/shinkai-tools-runner/build.rs
+++ libs/shinkai-tools-runner/build.rs
@@ -1,12 +1,13 @@
 use std::env;
 use std::fs;
 use std::path::Path;
+
 fn main() {
-    // Re-runs script if any files in res are changed
     println!("cargo:rerun-if-changed=resources/*");
 
     let resources_path = Path::new("shinka-tools-runner-resources");
-    let target_path = Path::new(&env::var("OUT_DIR").unwrap()).join("shinka-tools-runner-resources");
+    let target_path =
+        Path::new(&env::var("OUT_DIR").unwrap()).join("shinka-tools-runner-resources");
 
     if resources_path.exists() {
         fs::create_dir_all(&target_path).unwrap();
@@ -16,6 +17,24 @@ fn main() {
             let source = entry.path();
             let destination = target_path.join(file_name);
             fs::copy(source, destination).unwrap();
+        }
+    }
+
+    let backend_path = target_path.join("shinkai-tools-backend");
+
+    if backend_path.exists() {
+        #[cfg(unix)]
+        {
+            let mut permissions = fs::metadata(&backend_path).unwrap().permissions();
+            permissions.set_mode(0o755); // Set executable permissions for Unix
+        }
+        #[cfg(windows)]
+        {
+            let metadata = fs::metadata(&backend_path).unwrap();
+            let readonly = metadata.permissions().readonly();
+            if readonly {
+                fs::set_permissions(&backend_path, metadata.permissions()).unwrap();
+            }
         }
     }
 }
