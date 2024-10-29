3a1bd646ca26dde23f775681fc36705bc3a38f39
Alfredo Gallardo <agallardol@outlook.com>
2024-10-29 11:54:11 -0300

    - refactor: tools builder to contains simple and declarative types

--- libs/shinkai-tools-builder/src/index.ts
+++ libs/shinkai-tools-builder/src/index.ts
@@ -1,2 +1,31 @@
-export * from './base-tool';
-export * from './tool-definition';
+import * as Ajv from 'npm:ajv/dist/2019';
+
+export type BaseConfigurations = {} | Record<string, any>;
+export type BaseParameters = {} | Record<string, any>;
+export type BaseResult = {} | Record<string, any>;
+
+export type Run<
+  TConfigurations extends BaseConfigurations,
+  TParameters extends BaseParameters,
+  TResult extends BaseResult
+> = (
+  configurations: TConfigurations,
+  parameters: TParameters
+) => TResult | Promise<TResult>;
+
+export interface ToolDefinition<TRun extends Run<any, any, any>> {
+  id: string;
+  name: string;
+  description: string;
+  author: string;
+  keywords: string[];
+
+  configurations: Ajv.JSONSchemaType<Parameters<TRun>[0]>;
+  parameters: Ajv.JSONSchemaType<Parameters<TRun>[1]>;
+  result: Ajv.JSONSchemaType<ReturnType<TRun>>;
+
+  embedding_metadata?: {
+    model_name: string;
+    embeddings: number[];
+  };
+}
